c7d1941aa8d374b6cba34413c727721141b58b00
Keerthy <keerthy.vss@gmail.com>
2017-07-20 15:20:55 +0530

    Javascript Basic Tasks - javascript.info

---
+++ learning-js/variables.js
@@ -0,0 +1,18 @@
+/*let admin;
+let name = "John";
+admin = name;
+alert(admin);
+let str = 123;
+alert(typeof str);
+let age = null;
+alert(age);*/
+
+/*let str = null;
+console.log('Hello world');*/
+
+/*let a = '1' + 2 + 3;
+console.log(a);*/
+
+let counter = 1;
+let a = counter++;
+console.log(a);
